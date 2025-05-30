182f84d1e86c207504f65bb924662e4d0c3c5f71
maf013 <Magfrydenlund@gmail.com>
2025-05-30 15:22:20 +0200

    5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-/*
- * Write your own scripts in this file
- */
+// Lyt efter Enter/Return i søgefeltet
+document.getElementById('searchInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        window.location.href = 'findklub.html'; // Redirect til findklub
+    }
+});
